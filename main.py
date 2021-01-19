1ea752a1f9b247c30dc492e37ab2e82f9a57903e
Sofiaa-dotcom <72768448+Sofiaa-dotcom@users.noreply.github.com>
2021-01-19 11:11:50 -0500

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,4 @@
+while True: 
+     if input.light_level() > 2:
+         music.play_melody("E B C5 A B G A F ", 120)
+light.set_all(color.rgb(191, 229, 255))
---
+++ main.ts
@@ -0,0 +1,7 @@
+while (true) {
+    if (input.lightLevel() > 2) {
+        music.playMelody("E B C5 A B G A F ", 120)
+    }
+    
+}
+light.setAll(color.rgb(191, 229, 255))
